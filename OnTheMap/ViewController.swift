ab4ab8c3283739e4f504aeaabf4a617c85549b73
ulkoart <ulkoart@gmail.com>
2020-04-12 13:51:31 +0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by user on 12.04.2020.
+//  Copyright © 2020 ulkoart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
